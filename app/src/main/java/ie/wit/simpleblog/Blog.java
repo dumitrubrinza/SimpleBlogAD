6da3b87dfcd37cd8996a01c464b23a3d88950087
Dumitru <dimabrinza@gmail.com>
2017-02-24 00:50:12 +0000

    first SimpleBlog commit

---
+++ app/src/main/java/ie/wit/simpleblog/Blog.java
@@ -0,0 +1,48 @@
+package ie.wit.simpleblog;
+
+/**
+ * Created by User on 23/02/2017.
+ */
+
+public class Blog {
+
+    private String title;
+    private  String desc;
+    private String image;
+
+    public Blog(){
+    }
+
+    public Blog(String title, String desc, String image) {
+        this.title = title;
+        this.desc = desc;
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+
+
+}
